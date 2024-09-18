eb57e6012089fbdacf5d0fb05c7e958a6c94c89c
shivsingh2005 <148894515+shivsingh2005@users.noreply.github.com>
2024-09-18 11:51:03 +0530

    Update linkedlist.c

--- linkedlist.c
+++ linkedlist.c
@@ -4,7 +4,7 @@
 typedef struct node{
     int data;
     struct node *next;
-    
+
 }node;
 node*  takein(int n){
 node *head=NULL, *temp,*p;
@@ -12,7 +12,7 @@ int i;
 
 for(i=0;i<n;i++){
     temp=(node*)malloc(sizeof(node));
-    printf("enter %d elements of list",i);
+    printf("enter  elements of list",i);
     scanf("%d",&temp->data);
     temp->next=NULL;
     if(head==NULL){
@@ -32,27 +32,28 @@ void print(node *head)
 {
     node* temp=head;
     while(temp!=NULL){
-        printf("%d ",temp->data);
+        printf("%d ->",temp->data);
         temp=temp->next;
     }
+    printf(" NULL \n");
 }
 node *reverse(node* head){
     node* curr=head;
     node*prev=NULL;
     node *agla;
     while(curr!=NULL){
-        
+
         agla=curr->next;
         curr->next=prev;
         prev=curr;
         curr=agla;
-        
-        
+
+
     }
     head=prev;
     return head;
 
-    
+
 }
 void printsum(node *head){
     node* temp=head;
@@ -60,19 +61,32 @@ void printsum(node *head){
     while(temp!=NULL){
         sum=sum+ temp->data;
         temp=temp->next;
-        
+
     }
     printf("%d",sum);
 }
+node *insertathead(node *head,int data)
+{
+    node *temp=(node*)malloc(sizeof(node));
+    temp->data=data;
+    temp->next=head;
+    head=temp;
+    return head;
+}
 int main() {
+    int k;
     node* head;
     head= takein(3);
+    print(head);
     head=reverse(head);
 
-    print(head);
-   printsum(head);
+    //print(head);
+  // printsum(head);
     // Write C code here
-    printf("Try programiz.pro");
-
+   printf("enter data you want to insert");
+   scanf("%d",&k);
+head=insertathead(head, k);
+print(head);
     return 0;
 }
+
